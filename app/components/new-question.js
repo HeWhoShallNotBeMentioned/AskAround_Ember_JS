d195c7700548a7a071338ddfe3928533e82ce199
Terry Marr & Chris Underwood <student@epicodus.com>
2015-09-30 14:35:02 -0700

    open home and link to question

---
+++ app/components/new-question.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    addQuestion() {
+     var params = {
+       title: this.get("title"),
+       author: this.get("author"),
+       note: this.get("note")
+     };
+     this.sendAction('addQuestion', params);
+    }
+  }
+});
